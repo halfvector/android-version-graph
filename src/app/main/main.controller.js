0c6882d08fe58c7b088cdc0e19271668f8252398
Alex Barkan <alex@augmate.com>
2015-07-26 18:48:26 -0400

    real android versions

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,65 +9,67 @@
     function MainController($timeout, webDevTec, toastr) {
         var vm = this;
 
-        vm.awesomeThings = [];
+        var dateFormat = d3.time.format("%Y-%m-%d");
 
-        var layers = [
-            [
-                { x: 0, y: 6 },
-                { x: 1, y: 6 },
-                { x: 2, y: 6 },
-                { x: 3, y: 5 },
-                { x: 4, y: 4 },
-                { x: 5, y: 1 },
-                { x: 6, y: 0 },
-                { x: 7, y: 0 }
-            ], [
-                { x: 0, y: 0 },
-                { x: 1, y: 0 },
-                { x: 2, y: 0 },
-                { x: 3, y: 1 },
-                { x: 4, y: 2 },
-                { x: 5, y: 4 },
-                { x: 6, y: 3 },
-                { x: 7, y: 1 }
-            ], [
-                { x: 0, y: 0 },
-                { x: 1, y: 0 },
-                { x: 2, y: 0 },
-                { x: 3, y: 0 },
-                { x: 4, y: 0 },
-                { x: 5, y: 1 },
-                { x: 6, y: 3 },
-                { x: 7, y: 5 }
-            ]
-        ];
+        d3.csv('assets/android_versions.csv', function(data) {
+            var keys = d3.range(2, 22); // api version numbers
+            var versions = [];
 
-        buildGraph();
+            data.forEach(function(d) {
+                // each row contains multiple columns, where each column is a version layer
+                keys.forEach(function(k) {
+                    versions.push({
+                            version: k,
+                            x: dateFormat.parse(d.Date),
+                            y: +d[k]
+                        });
+                });
+            });
 
-        function buildGraph() {
+            var numSamples = 65; //versionMap[keys[0]].length;
+            buildGraph(keys, versions, numSamples);
+        });
 
-            var n = 3, // number of layers
-                m = 8, // number of samples per layer
-                stack = d3.layout.stack();//.offset("wiggle");
+        function buildGraph(keys, versions, numSamples) {
+            var n = keys.length, // number of layers
+                m = numSamples; // number of samples per layer
 
-            var versionsLayer = stack(layers);
+            console.log("n = " + n);
+            console.log("m = " + m);
 
-            var width = 1200,
-                height = 500;
+            var nest = d3.nest().key(function(d) {
+                return d.version;
+            });
 
-            var x = d3.scale.linear()
-                .domain([0, m - 1])
-                .range([0, width]);
+            var stack = d3.layout.stack()
+                // .offset("wiggle")
+                .values(function(d) {
+                    return d.values;
+                });
+
+            var versionsLayer = stack(nest.entries(versions));
+
+            var margin = {
+                    top: 20,
+                    right: 20,
+                    bottom: 30,
+                    left: 20
+                },
+                width = 1000 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+            var x = d3.time.scale()
+                // .domain([0, m - 1])
+                .domain(d3.extent(versions, function(d) {
+                    return d.x;
+                }))
+                .range([0, width - margin.left - margin.right]);
 
             var y = d3.scale.linear()
-                .domain([0, d3.max(versionsLayer, function(layer) {
-                    return d3.max(layer, function(d) {
-                        return d.y0 + d.y;
-                    });
-                })])
+                .domain([0, 100])
                 .range([0, height]);
 
-            var color = d3.scale.category10();
+            var color = d3.scale.category20c();
 
             var area = d3.svg.area()
                 .x(function(d) {
@@ -80,81 +82,47 @@
                     return y(d.y0 + d.y);
                 });
 
-            var svg = d3.select("body").append("svg")
+            var svg = d3.select("body #versions-chart")
+                .append("svg")
                 .attr("width", width)
-                .attr("height", height);
+                .attr("height", height + margin.bottom + margin.top)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .attr("width", width - margin.left - margin.right)
+                .attr("height", height + margin.bottom + margin.top);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .tickSize(height + 10)
+                .ticks(7);
 
             svg.selectAll("path")
                 .data(versionsLayer)
-                .enter().append("path")
-                .attr("d", area)
+                .enter()
+                .append("path")
+                .attr("class", "segment")
+                .attr("d", function(d) {
+                    return area(d.values);
+                })
                 .style("fill", function() {
                     return color(Math.random());
+                })
+                .append("title")
+                .text(function(d) {
+                    return "version: " + d.key;
                 });
 
-            // Inspired by Lee Byron's test data generator.
-            function bumpLayer(n) {
 
-                function bump(a) {
-                    var x = 1 / (.1 + Math.random()),
-                        y = 2 * Math.random() - .5,
-                        z = 10 / (.1 + Math.random());
-                    for (var i = 0; i < n; i++) {
-                        var w = (i / n - y) * z;
-                        a[i] += x * Math.exp(-w * w);
-                    }
-                }
-
-                var a = [],
-                    i;
-                for (i = 0; i < n; ++i) a[i] = 0;
-                for (i = 0; i < 5; ++i) bump(a);
-                return a.map(function(d, i) {
-                    return {
-                        x: i,
-                        y: Math.max(0, d)
-                    };
-                });
-            }
-        }
-
-        /* Inspired by Lee Byron's test data generator. */
-        function stream_layers(n, m, o) {
-            if (arguments.length < 3) o = 0;
-
-            function bump(a) {
-                var x = 1 / (.1 + Math.random()),
-                    y = 2 * Math.random() - .5,
-                    z = 10 / (.1 + Math.random());
-                for (var i = 0; i < m; i++) {
-                    var w = (i / m - y) * z;
-                    a[i] += x * Math.exp(-w * w);
-                }
-            }
-            return d3.range(n).map(function() {
-                var a = [],
-                    i;
-                for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-                for (i = 0; i < 5; i++) bump(a);
-                return a.map(stream_index);
-            });
-        }
-
-        /* Another layer generator using gamma distributions. */
-        function stream_waves(n, m) {
-            return d3.range(n).map(function(i) {
-                return d3.range(m).map(function(j) {
-                    var x = 20 * j / m - i / 3;
-                    return 2 * x * Math.exp(-.5 * x);
-                }).map(stream_index);
-            });
-        }
-
-        function stream_index(d, i) {
-            return {
-                x: i,
-                y: Math.max(0, d)
-            };
+            var xAxisGeometry = svg.append("g")
+                .attr("class", "x axis")
+                // .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .selectAll("g")
+                .filter(function(d) {
+                    return d;
+                })
+                .classed("minor", true);
         }
     }
 })();
